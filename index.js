35daf321e16c6505e7b14bf758a7b3b65e931f65
Samuel Adedeji <djsam2828@gmail.com>
2025-12-06 21:39:29 +0100

    Updated

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use('/images', express.static('upload/images'))
 app.post("/upload", upload.single('product'),(req, res)=>{
     res.json({
         success:1,
-        image_url: `https://shopper-backend-t7hv.onrender.com/${req.file.filename}` 
+        image_url: `https://shopper-backend-t7hv.onrender.com/images/${req.file.filename}` 
     })
 })
 
